b927e643bd954ea71ae8b6f7b8edb040941c83dd
Hoora <hoora@Hooras-MacBook-Pro.local>
2023-11-01 14:47:12 -0600

    add duty details

---
+++ TaskManagementSystem.WEB/Controllers/DutyController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagementSystem.BLL.Interfaces;
+using TaskManagementSystem.Lib.Models;
+
+namespace TaskManagementSystem.WEB.Controllers
+{
+    public class DutyController : Controller
+    {
+        private readonly IDutyRepository _dutyRepository;
+
+        public DutyController(IDutyRepository dutyRepository)
+        {
+            _dutyRepository = dutyRepository;
+        }
+
+
+        // GET: /duty/index
+        public async Task<ViewResult> Index(int id)
+        {
+            IList<Duty> duties = await _dutyRepository.GetAllDuties();
+            return View(duties);
+        }
+
+        // GET: /duty/details/id
+        public async Task<ViewResult> Details(int id)
+        {
+            Duty duty = await _dutyRepository.GetDuty(id);
+            ViewData["Header"] = "Task Details:";
+            ViewData["Duty"] = duty;
+            return View();
+        }
+    }
+}
+
